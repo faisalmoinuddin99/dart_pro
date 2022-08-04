0b40f1ef9643abdad2e250629d486556624464f1
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-08-04 19:57:39 +0530

    Number, String, Boolean DataTypes

--- bin/main.dart
+++ bin/main.dart
@@ -1,9 +1,31 @@
+// Exploring Data Types and Variables
+/*
+* Dart has special support for these data types
+* 1. Numbers: int, double
+* 2. Strings
+* 3. Booleans
+* 4. List (also known as Arrays)
+* 5. Maps
+* 6. Runes (for expressing Unicode characters in String)
+* 7. Symbols
+*
+* --> NOTE: All data types in Dart are Objects
+*           Therefore, values are by default 'null'
+* */
 void main(){
 
-  // single line comments
+    // Numbers: int
+    int age = 10 ;
 
-  /*
-  * Multi-line comments
-  */
+    // Numbers: double
+    var myAge = 93.4 ; // It is inferred as integer automatically
+
+    //String
+    String name = "Henry" ;
+    var myName = "faisal" ;
+
+    // Boolean
+    bool isVerified = true ;
+    var isAlive = false ;
 
 }
