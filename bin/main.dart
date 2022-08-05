a8e844dff8cc1132a1852098d5eba4e660b2a723
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-08-05 16:01:29 +0530

    Dart Short Hand Syntax | FAT Arrow | Function Expressions

--- bin/main.dart
+++ bin/main.dart
@@ -1,34 +1,15 @@
-// Exploring Functions or Methods
-/*
-Collection of statements grouped together to perform an operation
- */
+// Expression in Functions : SHORT HAND SYNTAX
+
+
 
 void main(){
-  int areaOfRectangle =  findArea(5,7) ;
-  print('$areaOfRectangle');
-
-  print(findCube(10,20,30)) ;
+  int perimeter = findPerimeter(10, 20) ;
+  print('$perimeter');
+  findArea(10,100) ;
 }
 
-int findArea(int length, int breadth) {
-  // Function Body: Put your code here
-  return length * breadth;
-}
-/*
-Properties of Functions in Dart
--> Functions in Dart are objects.
-          - Functions can be assigned to a variable or passed as parameter to
-          other function
--> All functions in Dart returns a value
-          - If no return value is specified the function return 'null'
--> Specifying return type is optional but is recommended as per code convention
- */
+int findPerimeter(int length, int breadth) => 2 * (length + breadth) ;
+void findArea(int length, int breadth) => print(length * breadth) ;
 
 
-
- findCube(int length, int width, int height) {
-
-   // by default, return null
-
- }
 
