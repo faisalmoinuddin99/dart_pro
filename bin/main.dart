e52d5570035747a3ea6425c9f94ee211d42dc55d
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-08-05 11:57:25 +0530

    Dart Functions or Methods Syntax and Properties

--- bin/main.dart
+++ bin/main.dart
@@ -1,16 +1,34 @@
-// for loop - definite iterator
+// Exploring Functions or Methods
+/*
+Collection of statements grouped together to perform an operation
+ */
 
 void main(){
-  for(int i = 1; i <= 10;  i++){
-    if(i % 2 == 0) {
-      print('$i');
-    }
-  }
+  int areaOfRectangle =  findArea(5,7) ;
+  print('$areaOfRectangle');
 
-  // for ..in loop
-  List<String> planets = ["Earth","Mars","Saturn","Jupyter"] ;
-
-  for(String planet in planets){
-    print(planet);
-  }
+  print(findCube(10,20,30)) ;
 }
+
+int findArea(int length, int breadth) {
+  // Function Body: Put your code here
+  return length * breadth;
+}
+/*
+Properties of Functions in Dart
+-> Functions in Dart are objects.
+          - Functions can be assigned to a variable or passed as parameter to
+          other function
+-> All functions in Dart returns a value
+          - If no return value is specified the function return 'null'
+-> Specifying return type is optional but is recommended as per code convention
+ */
+
+
+
+ findCube(int length, int width, int height) {
+
+   // by default, return null
+
+ }
+
