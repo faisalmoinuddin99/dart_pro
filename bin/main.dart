099d5bc132d98183f5c61088f7b816d6884c1595
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-08-05 16:17:45 +0530

    Dart Optional Positional | ? nullable | in Function

--- bin/main.dart
+++ bin/main.dart
@@ -1,15 +1,30 @@
-// Expression in Functions : SHORT HAND SYNTAX
+// Optional Positional Parameters
 
+/*
 
+Parameters : 1. Required
+             2. Optional
+                  a. Positional
+                  b. Named
+                  c. Default
+
+ */
 
 void main(){
-  int perimeter = findPerimeter(10, 20) ;
-  print('$perimeter');
-  findArea(10,100) ;
+  printCities('New York', 'Delhi', 'Sydney') ;
+  print("") ;
+  printCountries('USA') ;
 }
 
-int findPerimeter(int length, int breadth) => 2 * (length + breadth) ;
-void findArea(int length, int breadth) => print(length * breadth) ;
-
-
-
+// Required Parameters
+void printCities(String name1, String name2, String name3) {
+  print('Name 1 is $name1') ;
+  print('Name 2 is $name2') ;
+  print('Name 3 is $name3') ;
+}
+// Optional Positional
+void printCountries(String name1, [String? name2,String? name3]){
+  print('Name 1 is $name1') ;
+  print('Name 2 is $name2') ;
+  print('Name 3 is $name3') ;
+}
