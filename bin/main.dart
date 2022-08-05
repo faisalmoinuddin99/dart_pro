d05b54a5b9d78209fdac0aa91a2bf5d2896170ab
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-08-05 20:43:36 +0530

    Dart Class, Object, Instance and reference Variable

--- bin/main.dart
+++ bin/main.dart
@@ -1,28 +1,35 @@
-// Custom Exception Handling
+// Object Oriented Programming
 
-void main(){
+class Student {
+  late int id ; // Instance or Field Variable
+  late String name; // Instance or Field Variable
 
-  print('Case 5') ;
-  // CASE 5: Custom Exception
-  try{
-    depositMoney(-200) ;
-   }
-   on DepositException catch(e){
-    print(e.errorMessage()) ;
-   } finally{
-    print('Thanks for banking with us!!!') ;
+  void study() {
+    // code
+    print('$name is studying');
   }
+
+  void sleep() {
+    // code
+    print('$name is sleeping');
+  }
+
 }
 
-class DepositException implements Exception {
-  String errorMessage(){
-    return 'You cannot enter amount less than 0' ;
-  }
-}
+void main() {
 
-void depositMoney(int amount){
-  if(amount < 0){
-    throw  DepositException() ;
-  }
-}
+  Student student1 = Student(); // One Object, Student1 is reference variable
 
+  student1.name = 'kelvin' ;
+  student1.id = 12 ;
+  print('Name: ${student1.name}, RollNo: ${student1.id}') ;
+  student1.sleep() ; // kelvin is sleeping
+  student1.study() ; // kelvin is studying
+  print("") ;
+  Student student2 = Student() ; // One Object, Student1 is reference variable
+  student2.id = 11 ;
+  student2.name = 'Rahul' ;
+  print('Name: ${student2.name}, RollNo: ${student2.id}') ;
+  student2.sleep() ;
+  student2.study() ;
+}
