59b0ffe93189ce95809fe7eca6963f822e49070a
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-08-06 11:08:47 +0530

    Dart Constructors in Inheritance: Default and Named Constructor

--- bin/main.dart
+++ bin/main.dart
@@ -1,76 +1,56 @@
-// Inheritance
+// Objective
+// 1. Inheritance with Default Constructor and Parameterised Constructor
+// 2. Inheritance with Named Constructor
+
+void main() {
+  Dog dog1 = Dog("Labrador", "Brown");
+  print("");
+  Dog dog2 = Dog("Pug", "White");
+  print("");
+  Dog dog3 = Dog.myNamedConstructor("German Shepherd", "Black-Brown");
+}
+
+class Animal {
+  late String color;
+
+  Animal(this.color) {
+    print('Animal class constructor');
+  }
+
+  Animal.myAnimalNamedConstructor(String color) {
+    print('Animal class named constructor');
+  }
+}
+
+class Dog extends Animal {
+  late String breed;
+
+  Dog(String breed, String color) : super(color) {
+    print('Dog class constructor');
+  }
+
+  Dog.myNamedConstructor(String breed, String color)
+      : super.myAnimalNamedConstructor(color) {
+    print('Dog class Named Constructor');
+  }
+}
 
 /*
-Commonly used Inheritance: 1. Single Inheritance
-                           2. Multi-level Inheritance
-                           3. Hierarchical Inheritance
+* OUTPUT:
+Animal class named constructor
+Dog class constructor
 
--> Inheritance is a mechanism in which one object acquires properties of its
-parent class object
+Animal class named constructor
+Dog class constructor
 
--> Super class of any class is Object:
-    . Provides default implementation of:
-    -> toString(), returns the String representation of object
-    -> hashCode Getter, returns the Hash Code of an Object
-    -> operator ==, to compare two objects
+Animal class named constructor
+Dog class Named Constructor
+* */
 
-Advantage:
-    . Code reusability
-    . Method Overriding
-    . Cleaner code: no repetition
+/*
+NOTE: . By default, a constructor in a subclass calls the superclass no-argument
+      constructor.
+      . Parent class constructor is always called before child class constructor
+      . If default constructor is missing in Parent class, then you must manually
+      call one of the constructors in super class
  */
-class Animal {
-  late String color = "Brown" ;
-
-  void eat(){
-    print('Animal Eat !') ;
-  }
-}
-class Dog extends Animal{
-
-  late String breed ;
-  @override
-  late String color = "black" ;
-
-  void bark(){
-    print('Bark!!') ;
-  }
-  @override
-  void eat(){
-    super.eat() ;
-    print('pedigree') ;
-
-  }
-}
-
-class Cat extends Animal {
-
-  late int age ;
-
-  void meow(){
-    print('Meow !') ;
-  }
-  @override
-  void eat(){
-    print('Kit & Kaboodle Dry Cat Food') ;
-  }
-
-}
-
-void main(){
-  Dog dog = Dog() ;
-  dog.breed = "Labrador" ;
-  dog.bark() ;
-  dog.eat() ;
-  print(dog.color) ;
-
-  Cat cat = Cat() ;
-  cat.age = 4 ;
-  cat.color = "White" ;
-  cat.eat() ;
-  cat.meow() ;
-
-  Animal animal = Animal() ;
-  animal.color = "Brown" ;
-  animal.eat() ;
-}
